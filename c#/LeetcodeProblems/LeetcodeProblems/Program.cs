678eb5d24030bc098a8061dac2d7107b4fbd3431
Mixxy1987 <mixxy@yandex.ru>
2021-01-10 14:45:27 +0300

    1122

---
+++ c#/LeetcodeProblems/LeetcodeProblems/Program.cs
@@ -0,0 +1,16 @@
+﻿using LeetcodeProblemsLib;
+
+namespace LeetcodeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+
+            var res = s.RelativeSortArray(
+                new[] {2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19},
+                new[] {2, 1, 4, 3, 9, 6});
+        }
+    }
+}
---
+++ c#/LeetcodeProblems/LeetcodeProblemsLib/Solution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LeetcodeProblemsLib
+{
+    public class Solution
+    {
+        /// <summary>
+        /// N 1122
+        /// https://leetcode.com/problems/relative-sort-array/
+        /// </summary>
+        public int[] RelativeSortArray(int[] arr1, int[] arr2)
+        {
+            var res1 = new List<int>(arr1.Length);
+            var res2 = new List<int>();
+            var pr = new Dictionary<int, int>();
+            
+            for (var i = 0; i < arr2.Length; i++) pr[arr2[i]] = i;
+
+            for (var i = 0; i < arr1.Length; i++)
+                if (pr.ContainsKey(arr1[i])) res1.Add(arr1[i]);
+                else res2.Add(arr1[i]);
+
+            res1.Sort(new DuplicateKeyComparer(pr));
+            res2.Sort();
+            res1.AddRange(res2);
+            return res1.ToArray();
+        }
+
+        private struct DuplicateKeyComparer : IComparer<int>
+        {
+            private readonly Dictionary<int, int> _pr;
+            public DuplicateKeyComparer(Dictionary<int, int> pr) => _pr = pr;
+            public int Compare(int x, int y) => _pr[x] >= _pr[y] ? 1 : -1;
+        }
+    }
+}
